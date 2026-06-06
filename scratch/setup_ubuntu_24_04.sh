004a35f5c474c340f1a20c4d20f79f9359399f58
willwinthan <willwinthan@gmail.com>
2026-06-06 10:43:28 +0700

    fix: remove php7.4-json from compatibility script since it is built-in

--- scratch/setup_ubuntu_24_04.sh
+++ scratch/setup_ubuntu_24_04.sh
@@ -39,7 +39,7 @@ apt-get update
 echo -e "\n${YELLOW}[3/6] Installing PHP 7.4 FPM and extension packages...${NC}"
 apt-get install -y php7.4-fpm php7.4-cli php7.4-mysql php7.4-xml php7.4-gd \
                    php7.4-curl php7.4-mbstring php7.4-zip php7.4-bcmath \
-                   php7.4-json php7.4-sqlite3 php7.4-soap php7.4-intl
+                   php7.4-sqlite3 php7.4-soap php7.4-intl
 
 # Start and enable PHP 7.4 FPM
 systemctl start php7.4-fpm
